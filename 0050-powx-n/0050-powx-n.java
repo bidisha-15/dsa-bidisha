b5464b488f2a4fdbc6b4fe5d756182ee199d070b
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2024-09-14 19:53:26 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (76.25%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // if(n==0)
+        //     return 1.0;
+        // else 
+        //     return x*myPow(x,n-1);
+        return Math.pow(x,n);
+    }
+}
