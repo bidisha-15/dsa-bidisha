d9eaeb3428c7885007fe4a74ce2d6c2524ecaa80
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2025-02-28 21:29:56 +0530

    Time: 5 ms (63.46%), Space: 45.3 MB (52.84%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,33 +1,45 @@
+// class Solution {
+//     public String convert(String s, int numRows) {
+//         int i = 0;
+//         char carr[] = s.toCharArray();
+//         if (numRows == 1 ) {
+//             return s;
+//         }
+//         String arr[] = new String[numRows];
+//         while (i < s.length()) {
+//             for (int idx = 0; idx < numRows && i < s.length(); idx++) {
+//                 if(arr[idx]==null){
+//                     arr[idx]="";
+//                 }
+//                 arr[idx] = arr[idx] + carr[i++];
+//             }
+//             for (int idx = numRows - 2; idx >= 1 && i < s.length(); idx--) {
+//                 arr[idx] = arr[idx] + carr[i++];
+//             }
+//         }
+//         for (i = 1; i < arr.length; i++) {
+//             arr[0] = arr[0].concat(arr[i]);
+//         }
+//         return arr[0];
+//     }
+// }
+
 class Solution {
-    public String convert(String s, int numRows) {
-        int x = 1, f = 0;
-        int arr[] = new int[s.length()];
-        if(numRows==1){
-               return s;}
-        for (int i = 0; i < arr.length; i++) {
-        
-            
-            if (x == numRows) {
-                f = 1;
-            }
-            else if(x==1){
-                f=0;
-            }
-            arr[i] = x;
-            if (f == 0) {
-                x++;
-            } else {
-                x--;
-            }
-        }
-        String res="";
-        for(int i=1;i<=numRows;i++){
-            for(int j=0;j<arr.length;j++){
-                if(i==arr[j]){
-                    res=res+s.charAt(j);
-                }
-            }
-        }
-        return res;
+    public String convert(String s, int nRows) {
+    char[] c = s.toCharArray();
+    int len = c.length;
+    StringBuffer[] sb = new StringBuffer[nRows];
+    for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
+    
+    int i = 0;
+    while (i < len) {
+        for (int idx = 0; idx < nRows && i < len; idx++) // vertically down
+            sb[idx].append(c[i++]);
+        for (int idx = nRows-2; idx >= 1 && i < len; idx--) // obliquely up
+            sb[idx].append(c[i++]);
     }
+    for (int idx = 1; idx < sb.length; idx++)
+        sb[0].append(sb[idx]);
+    return sb[0].toString();
+}
 }
