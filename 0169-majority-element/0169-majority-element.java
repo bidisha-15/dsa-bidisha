d6c6587202b9e47509195d3d6f24a84095435a90
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2025-04-09 00:35:02 +0530

    Time: 3 ms (57.1%), Space: 53.3 MB (29.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,11 +1,8 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // if(nums.length==1){
-        //     return nums[0];
-        // }
-        int cnt=0;
-        int ele=nums[0];
-        int n=nums.length;
+        // int cnt=0;
+        // int ele=nums[0];
+        // int n=nums.length;
         Arrays.sort(nums);
         // for(int i=0;i<nums.length-1;i++){
         //     if(nums[i]==nums[i+1]){
@@ -17,7 +14,7 @@ class Solution {
         //     }
         // }
         // return ele;
-            return nums[(n/2)];
+            return nums[(nums.length/2)];
 
     }
 }
