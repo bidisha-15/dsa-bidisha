e0cd51dbba57dd381c53be4d1145e17a5530e3b2
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2025-04-09 00:44:26 +0530

    Time: 2 ms (75.01%), Space: 53.4 MB (21.31%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,36 @@
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         // int cnt=0;
+//         // int ele=nums[0];
+//         // int n=nums.length;
+//         Arrays.sort(nums);
+//         // for(int i=0;i<nums.length-1;i++){
+//         //     if(nums[i]==nums[i+1]){
+//         //         cnt++;
+//         //     }
+//         //     if(cnt>=Math.floor(n/2)){
+//         //         ele=nums[i];
+//         //         break;
+//         //     }
+//         // }
+//         // return ele;
+//             return nums[(nums.length/2)];
+
+//     }
+// }
 class Solution {
     public int majorityElement(int[] nums) {
-        // int cnt=0;
-        // int ele=nums[0];
-        // int n=nums.length;
-        Arrays.sort(nums);
-        // for(int i=0;i<nums.length-1;i++){
-        //     if(nums[i]==nums[i+1]){
-        //         cnt++;
-        //     }
-        //     if(cnt>=Math.floor(n/2)){
-        //         ele=nums[i];
-        //         break;
-        //     }
-        // }
-        // return ele;
-            return nums[(nums.length/2)];
-
+        int res = 0;
+        int majority = 0;
+        
+        for (int n : nums) {
+            if (majority == 0) {
+                res = n;
+            }
+            
+            majority += n == res ? 1 : -1;
+        }
+        
+        return res;        
     }
 }
