c06640d8c17fab0418b5fb65d3c413513288b7c2
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2025-04-14 00:21:27 +0530

    Time: 0 ms (100%), Space: 42.6 MB (18.39%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,17 +1,40 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int row=0, col=n-1;
-        while(row<m && col>=0)
-        {
-            if(target==matrix[row][col])
-                return true;
-            else if(target<matrix[row][col])
-                col--;
-            else if(target>matrix[row][col])
-                row++;       
-        }
-        return false;
+        int low=0;
+        int high=matrix.length-1;
+        int mid=0;
+        int found=-1;
+            while(low<=high){
+                mid=low+(high-low)/2;
+                if(target>=matrix[mid][0] && target<=matrix[mid][matrix[mid].length-1]){
+                    found=mid;
+                    break;
+                }
+                else if(target<matrix[mid][0]){
+                    high=mid-1;
+                }
+                else if(target>matrix[mid][matrix[mid].length-1]){
+                    low=mid+1;
+                }
+            }
+            if(found==-1){
+                return false;
+            }
+        low=0;
+        high=matrix[found].length-1;
+        mid=0;
+            while(low<=high){
+                mid=low+(high-low)/2;
+                if(target==matrix[found][mid]){
+                    return true;
+                }
+                else if(target<matrix[found][mid]){
+                    high=mid-1;
+                }
+                else if(target>matrix[found][mid]){
+                    low=mid+1;
+                }
+            }
+            return false;
     }
 }
