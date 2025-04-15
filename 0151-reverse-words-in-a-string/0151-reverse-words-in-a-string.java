9f0da3243ee9f1f6524e7ca741a45c4129fdb9b5
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2025-04-16 01:20:03 +0530

    Time: 11 ms (20.91%), Space: 45.6 MB (7.32%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -4,9 +4,13 @@ class Solution {
         String[] myArray = s.split(" ");
         String res="";
     for (int i=myArray.length-1;i>=0;i--) {
+        if(i!=0){
         res=res+myArray[i]+" ";
+        }
+        else{
+        res=res+myArray[i];
+        }
     }
-    res=res.trim();
     return res;
     }
 }
