a494a288511716157554cf546c88995567874e95
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2025-07-12 01:57:52 +0530

    Time: 1 ms (99.89%), Space: 46.4 MB (35.45%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=1;i<nums.length;i=i+2){
-            if(nums[i-1]!=nums[i]){
-                return nums[i-1];
-            }
+        int sum=0;
+        for(int n:nums){
+            sum = sum ^ n;
         }
-        return nums[nums.length-1];
+        return sum;
     }
 }
