816a83010c6b96a91506cf5d376c2c6645b52609
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2025-07-13 00:27:49 +0530

    Time: 0 ms (100%), Space: 42.1 MB (75.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,9 +1,21 @@
 class Solution {
-    public double myPow(double x, int n) {
-        // if(n==0)
-        //     return 1.0;
-        // else 
-        //     return x*myPow(x,n-1);
-        return Math.pow(x,n);
+    public double myPow(double x, long n) {
+        double res=1;
+        boolean sign=false;
+        if(n<0){
+            n=-n;
+            sign=true;
+        }
+        while(n>0){
+            if((n&1)==1){
+                res = res*x;
+            }
+            x*=x;
+            n= n >> 1;
+        }
+        if(sign){
+            return (1/res);
+        }
+        return res;
     }
 }
