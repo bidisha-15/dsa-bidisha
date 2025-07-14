dc9da105addcd55426b079321d832418efbe8b07
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2025-07-15 02:03:18 +0530

    Time: 92 ms (88.58%), Space: 49.3 MB (59.01%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean sieve[]=new boolean[n+1];
+        if(n<2){
+            return 0;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(!sieve[i]){
+                for(int j=i+i;j<=n;j+=i){
+                    sieve[j]=true;
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=2;i<n;i++){
+            if(!sieve[i]){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
