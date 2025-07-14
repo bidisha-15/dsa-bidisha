b46a4aae835d0aa340795fedf6799a07430eca26
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2025-07-14 23:41:30 +0530

    Time: 0 ms (100%), Space: 40.8 MB (89.68%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -1,18 +1,11 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int n = nums.length;
-        int totalSum = 0;
-        // Iterate through all possible subsets
-        for (int i = 0; i < (1 << n); i++) {
-            int subsetXor = 0;
-            for (int j = 0; j < n; j++) {
-                // Check if the j-th element is in the i-th subset
-                if ((i & (1 << j)) != 0) {
-                    subsetXor ^= nums[j];
-                }
-            }
-            totalSum += subsetXor;
+        int result = 0;
+        // Capture each bit that is set in any of the elements
+        for (int num : nums) {
+            result |= num;
         }
-        return totalSum;
+        // Multiply by the number of subset XOR totals that will have each bit set
+        return result << (nums.length - 1);
     }
 }
