aa4c91c01a44847abe24a428a5f08743fc355052
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2025-07-17 20:47:22 +0530

    Time: 22 ms (11.46%), Space: 42.7 MB (36.48%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.equals(t)){
-            return true;
-        }
+        char s1[]=s.toCharArray();
+        char t1[]=t.toCharArray();
         HashMap<Character, Character> ht = new HashMap<>();
         HashMap<Character, Character> ht1 = new HashMap<>();
         for(int i=0;i<s.length();i++){
-            if (ht.containsKey(s.charAt(i)) && ht.get(s.charAt(i)) != t.charAt(i)) {
+            if (ht.containsKey(s1[i]) && ht.get(s1[i]) != t1[i]) {
                 return false;
             }
-            if (ht1.containsKey(t.charAt(i)) && ht1.get(t.charAt(i)) != s.charAt(i)) {
+            if (ht1.containsKey(t1[i]) && ht1.get(t1[i]) != s1[i]) {
                 return false;
             }
             // if((ht.containsKey(t.charAt(i)) && ht.get(t.charAt(i)) == t.charAt(i))){return false;}
-            ht.put(s.charAt(i), t.charAt(i));
-            ht1.put(t.charAt(i), s.charAt(i));
+            ht.put(s1[i], t1[i]);
+            ht1.put(t1[i], s1[i]);
             
         }
         return true;
