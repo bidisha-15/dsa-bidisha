b255e63bb73cdf2ebf2ec8ec11b26a2d50f7cede
Bidisha Banik <116992974+bidisha-15@users.noreply.github.com>
2025-08-20 19:57:23 +0530

    Time: 2 ms (83.17%), Space: 44.7 MB (64.26%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
